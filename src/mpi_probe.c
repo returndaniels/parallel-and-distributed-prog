0e40c231ac9eccfb8eb9a3beedde3664e81da290
Bernardo Abreu <bprado.abreu@gmail.com>
2023-09-12 15:57:12 -0300

    first commit

--- src/mpi_probe.c
+++ src/mpi_probe.c
@@ -1,13 +1,13 @@
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX 1000
+#define MAX 10000
 
 int main(int argc, char *argv[]) { /* mpi_probe.c  */
-int meu_ranque, num_procs, etiq=0;
-int* vet_num;
-int total_num, origem, destino=1, numeros[MAX];
-MPI_Status estado;
+    int meu_ranque, num_procs, etiq=0;
+    int* vet_num;
+    int total_num, origem=0, destino=1, *numeros;
+    MPI_Status estado;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &meu_ranque);
@@ -16,6 +16,10 @@ MPI_Status estado;
     if (meu_ranque == origem) {
         srand(MPI_Wtime());
         total_num = (rand() / (float)RAND_MAX) * MAX;
+        numeros = (int*) malloc(sizeof(int)*total_num);
+	for(int i = 0; i < total_num; i++){
+		numeros[i]=i;
+	}
         MPI_Send(numeros, total_num, MPI_INT, destino, etiq, MPI_COMM_WORLD);
         printf("Processo %d enviou %d números  para processo %d.\n", origem, total_num, destino);
     } 
@@ -31,6 +35,11 @@ MPI_Status estado;
 	    etiq = estado.MPI_TAG;
     /* Agora recebe a mensagem com o vetor previamente alocado  */
             MPI_Recv(vet_num, total_num, MPI_INT, origem, etiq, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	    for(int i = 0; i < total_num; i++){
+		printf("%d ", vet_num[i]);
+	       	
+	    }
+	    puts("\n");
         printf("Processo %d recebeu dinamicamente %d números de %d.\n", destino, total_num, origem);
             free(vet_num);
         }
--- src/mpi_scatter.c
+++ src/mpi_scatter.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "mpi.h"
-#define TAM_VET 10
+#define TAM_VET 20
 
 int main(int argc, char *argv[]) { /* mpi_scatter.c  */
 int i, meu_ranque, num_procs, raiz = 0;
-int *vet_envia, vet_recebe[TAM_VET];
+double *vet_envia, vet_recebe[TAM_VET], j;
 MPI_Comm com=MPI_COMM_WORLD;
 	
 	MPI_Init(&argc, &argv);
@@ -13,16 +13,16 @@ MPI_Comm com=MPI_COMM_WORLD;
     	MPI_Comm_size(com, &num_procs);
     	/* O processo raiz aloca o espaço de memória e inicia o vetor */
     	if (meu_ranque == raiz) {
-        	vet_envia = (int*) malloc (num_procs*TAM_VET*sizeof(int));
-        	for (i = 0; i < (TAM_VET*num_procs); i++) 
-             		vet_envia[i] = i;
+        	vet_envia = (double*) malloc (num_procs*TAM_VET*sizeof(double));
+        	for (i = 0, j=0; i < (TAM_VET*num_procs); i++, j++) 
+             		vet_envia[i] = j;
     	}
     	/* O vetor é distribuído em partes iguais entre os processos, incluindo o processo raiz */
-    	MPI_Scatter(vet_envia, TAM_VET, MPI_INT, vet_recebe, TAM_VET, MPI_INT, raiz, com);
+    	MPI_Scatter(vet_envia, TAM_VET, MPI_DOUBLE, vet_recebe, TAM_VET, MPI_DOUBLE, raiz, com);
 	/* Cada processo imprime a parte que recebeu */ 
 	printf("Processo =  %d, recebeu", meu_ranque);
 	for (i = 0; i < TAM_VET; i ++) 
-		printf(" %d", vet_recebe[i]);
+		printf(" %lf", vet_recebe[i]);
 	printf("\n");
 	/* Termina a execução */
 	MPI_Finalize();
