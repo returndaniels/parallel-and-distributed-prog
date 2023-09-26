2eeca24e88d84d39f0293377fd7343a7ab44a31b
Daniel Silva <returndaniels@gmail.com>
2023-09-26 16:36:38 -0300

    Acrescentado default(none) em omp_shared.c

--- OPENMP/src/omp_shared.c
+++ OPENMP/src/omp_shared.c
@@ -9,7 +9,7 @@ int a[n];
     (void) omp_set_num_threads(4);
     for (i = 0; i < n; i++)
         a[i] = i+1;
-    #pragma omp parallel for shared(a)
+    #pragma omp parallel for default(none) shared(a, n)
     for (i = 0; i < n; i++) {
         a[i] += i;
     } /*-- Fim do parallel for --*/
