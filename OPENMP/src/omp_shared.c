52b760d58e5c13968536fb92f50b1876e95b5e43
Daniel Silva <returndaniels@gmail.com>
2023-09-26 16:41:52 -0300

    Testes em omp_shared

--- OPENMP/src/omp_shared.c
+++ OPENMP/src/omp_shared.c
@@ -3,15 +3,16 @@
 #include <omp.h>
 
 int main(int argc, char *argv[]) { /* omp_shared.c  */
-int i, n = 7;
+int i, j, n = 7;
 int a[n];
 
     (void) omp_set_num_threads(4);
     for (i = 0; i < n; i++)
         a[i] = i+1;
-    #pragma omp parallel for default(none) shared(a, n)
+    #pragma omp parallel for default(none) shared(a, n) private(j)
     for (i = 0; i < n; i++) {
-        a[i] += i;
+        for (j = 0; j <= i; j++) 
+            a[i] += i;
     } /*-- Fim do parallel for --*/
     printf("No programa principal depois do parallel for:\n");
     for (i = 0; i < n; i++)
