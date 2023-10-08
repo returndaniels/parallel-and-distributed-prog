f118d947e92a10f600b85c855c17beedab71e614
Daniel Silva <returndaniels@gmail.com>
2023-10-08 18:42:10 -0300

    Correção no uso do  MPI_Isend

--- Trabalho 1/problema 3/mpi_least_squares_Isend.c
+++ Trabalho 1/problema 3/mpi_least_squares_Isend.c
@@ -12,7 +12,6 @@ int main(int argc, char **argv)
   int i, j, n, myid, numprocs, naverage, nremain, mypoints, ishift;
   double start_time, end_time;
   MPI_Status istatus;
-  MPI_Request x_request, y_request;
   FILE *infile;
 
   infile = fopen("xydata", "r");
@@ -46,24 +45,29 @@ int main(int argc, char **argv)
     y = (double *)malloc(n * sizeof(double));
   }
 
-  // Passo 2: O Processo 0 envia subconjuntos de x e y
+  // Passo 2: O Processo 0 envia subconjuntos de x e y usando MPI_Isend
   naverage = n / numprocs;
   nremain = n % numprocs;
   if (myid == 0)
   {
+    MPI_Request *requests = (MPI_Request *)malloc(numprocs * sizeof(MPI_Request));
+    MPI_Status *statuses = (MPI_Status *)malloc(numprocs * sizeof(MPI_Status));
+
     for (i = 1; i < numprocs; i++)
     {
       ishift = i * naverage;
       mypoints = (i < numprocs - 1) ? naverage : naverage + nremain;
-
-      // Usa MPI_Isend para enviar os dados de x e y
-      MPI_Isend(&x[ishift], mypoints, MPI_DOUBLE, i, 3, MPI_COMM_WORLD, &x_request);
-      MPI_Isend(&y[ishift], mypoints, MPI_DOUBLE, i, 4, MPI_COMM_WORLD, &y_request);
-
-      // Espera pela conclusão das operações de envio
-      MPI_Wait(&x_request, &istatus);
-      MPI_Wait(&y_request, &istatus);
+      MPI_Isend(&ishift, 1, MPI_INT, i, 1, MPI_COMM_WORLD, &requests[i - 1]);
+      MPI_Isend(&mypoints, 1, MPI_INT, i, 2, MPI_COMM_WORLD, &requests[i - 1]);
+      MPI_Isend(&x[ishift], mypoints, MPI_DOUBLE, i, 3, MPI_COMM_WORLD, &requests[i - 1]);
+      MPI_Isend(&y[ishift], mypoints, MPI_DOUBLE, i, 4, MPI_COMM_WORLD, &requests[i - 1]);
     }
+
+    // Aguarda a conclusão de todos os envios sem bloqueio
+    MPI_Waitall(numprocs - 1, requests, statuses);
+
+    free(requests);
+    free(statuses);
   }
   else
   {
